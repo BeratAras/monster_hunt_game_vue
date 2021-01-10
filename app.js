6605591f2c58ecc059b7dd53e88e8abc20a82ca2
Berat Aras <m.berataras@gmail.com>
2021-01-10 21:15:10 +0300

    -game over kontrolleri. -pes et kontorlü -özel saldırı ve normal saldırı. -ilk yardım kontrolü. -css düzenlemesi.

--- app.js
+++ app.js
@@ -10,16 +10,47 @@ new Vue({
             this.game_is_on = true;
         },
         attack(){
-
+            var point = Math.ceil(Math.random() * 10);
+            this.monster_heal -= point;
+            this.monster_attack();
+            this.is_game_over();
         },
         special_attack(){
-            
+            var point = Math.ceil(Math.random() * 20);
+            this.monster_heal -= point;
+            this.monster_attack();
+            this.is_game_over();
         },
         heal_up(){
-
+            this.player_heal += 20;
+            if(this.player_heal == 100 || this.player_heal > 100){
+                alert('Daha Fazla Can Alamazsın');
+                this.player_heal -= 20;
+            }
+            this.monster_attack();
         },
         give_up(){
-
+            alert('Pes Ettiniz. Kazanan Canavar');
+            this.game_is_on = false;
+            this.game_over();
+        },
+        monster_attack(){
+            var point = Math.ceil(Math.random() * 10);
+            this.player_heal -= point;
+        },
+        is_game_over(){
+            if(this.player_heal < 0){
+                alert('Canavar Kazandı.');
+                this.game_over();
+            }else if(this.monster_heal < 0){
+                alert('Kazandın. Canavar Geberdi!');
+                this.game_over();
+            }
+        },
+        game_over(){
+            this.player_heal = 100;
+            this.monster_heal = 100;
+            this.game_is_on = false;
         }
     }
 })
