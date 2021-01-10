bae979ec9afdd5c1a8f0090b3cbd0eb3e489f344
Berat Aras <m.berataras@gmail.com>
2021-01-10 22:33:38 +0300

    log alma oluşturuldu.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
         player_heal: 100,
         monster_heal: 100,
         game_is_on: false,
+        logs: [],
     },
     methods: {
         start_game(){
@@ -12,12 +13,14 @@ new Vue({
         attack(){
             var point = Math.ceil(Math.random() * 10);
             this.monster_heal -= point;
+            this.add_to_log({ turn: 'p', text: "Oyuncu " + point + " Hasar Verdi." });
             this.monster_attack();
             this.is_game_over();
         },
         special_attack(){
             var point = Math.ceil(Math.random() * 20);
             this.monster_heal -= point;
+            this.add_to_log({ turn: 'p', text: "Oyuncu " + point + " Hasar Verdi." });
             this.monster_attack();
             this.is_game_over();
         },
@@ -37,6 +40,7 @@ new Vue({
         monster_attack(){
             var point = Math.ceil(Math.random() * 10);
             this.player_heal -= point;
+            this.add_to_log({ turn: 'm', text: "Canavar " + point + " Hasar Verdi." });
         },
         is_game_over(){
             if(this.player_heal < 0){
@@ -51,6 +55,9 @@ new Vue({
             this.player_heal = 100;
             this.monster_heal = 100;
             this.game_is_on = false;
+        },
+        add_to_log(log){
+            this.logs.push(log);
         }
     }
 })
